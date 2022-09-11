127613fa04709d5cedea5215a4b1e9b976eb1169
Jonathan Christian <christianj707@gmail.com>
2022-09-11 14:56:34 -0700

    Create makeGrid function

--- main.js
+++ main.js
@@ -1,17 +1,45 @@
 let gridSizeBtn = document.querySelector("#grid-size-btn");
 let eraseBtn = document.querySelector("#erase-btn");
+let grid = document.querySelector('.grid-main-body-grid');
+let newDivs = document.querySelectorAll('.newDiv');
+let innerDivs = document.querySelectorAll('.innerDiv');
 
-function makeGrid() {
-
+function makeGrid(num) {
+  height = 550/num;
+  width = 550/num;
+  for (i = 0; i < num; i++) {
+    let newDiv = document.createElement('div');
+    newDiv.classList.add("newDiv");
+    newDiv.style.margin= "0";
+    newDiv.style.height = `${height}px`;
+    newDiv.style.width = `550px`;
+    grid.appendChild(newDiv);
+    for (j = 0; j < num; j++) {
+      innerDiv = document.createElement('div');
+      innerDiv.classList.add("innerDiv");
+      innerDiv.style.margin= "0";
+      innerDiv.style.height = `${height}px`;
+      innerDiv.style.width = `${width}px`;
+      newDiv.appendChild(innerDiv);
+    }
+  }
+  
 }
 
+window.onload = makeGrid(16);
+
 function changeGridSize() {
-  window.prompt('Enter a number between 16 and 64 to change the grid size.');
+  let num = window.prompt('Enter a number between 1 and 50 to change the grid size.');
+  makeGrid(num);
 }
 
 function erase() {
-  window.alert('It works');
+  window.alert('It worked');
 }
+
+grid.addEventListener('mouseover', function() {
+  //innerDiv.style.backgroundColor = 'black';
+});
 
 gridSizeBtn.addEventListener('click', function() {
   changeGridSize();
@@ -19,4 +47,4 @@ gridSizeBtn.addEventListener('click', function() {
 
 eraseBtn.addEventListener('click', function() {
   erase();
-})
+});
